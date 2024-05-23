0615e36ee5b33d972f4f7b7987206028ec70ec33
Alberto Muñiz <92947424+AlbMP96@users.noreply.github.com>
2024-05-23 23:59:17 +0200

    Added styles

--- resources/js/Pages/Guide.vue
+++ resources/js/Pages/Guide.vue
@@ -17,7 +17,7 @@ defineProps({
 </script>
 
 <template>
-    <Head title="News" />
+    <Head title="Create Guide" />
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <div
                 class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
@@ -25,7 +25,19 @@ defineProps({
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <Header :canLogin="canLogin" :canRegister="canRegister"/>
                     <main class="mb-10">
-                       <QuillEditor toolbar="essential" class="text-black bg-white mx-5"/>
+                        <div class="flex flex-col justify-center items-center">
+                            <h1 class="text-3xl text-center m-5 underline font-bold">Create your guide</h1>
+                            <div class="my-3 text-center">
+                                <label for="title">Title: </label>
+                                <input id="title" type="text" class="bg-gray-100 text-black"/>
+
+                                <select name="ff-class" id="ff-class" class="mx-3 text-black">
+                                    <option value="default" hidden selected>Select Class</option>
+                                </select>
+                            </div>
+                            <QuillEditor toolbar="essential" class="text-black bg-gray-100 min-h-screen w-screen"/>
+                            <input type="submit" value="Create" class="justify-center my-10 px-5 py-2 rounded-lg text-black bg-red-600 hover:cursor-pointer hover:bg-red-500">
+                        </div>
                     </main>
                </div>
             </div>
--- resources/js/Pages/News.vue
+++ resources/js/Pages/News.vue
@@ -25,6 +25,7 @@ defineProps({
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <Header :canLogin="canLogin" :canRegister="canRegister"/>
                     <main>
+                        <h1 class="text-3xl underline font-bold text-center m-5">Latest News</h1>
                         <div v-for="item in news" :key="item.id" class="text-center text-white border-4 border-red-500 rounded p-3.5 my-3.5">
                             <a :href="item.url" target="_blank">
                                 <p class="text-2xl underline mb-2 decoration-2">{{ item.title }}</p>
