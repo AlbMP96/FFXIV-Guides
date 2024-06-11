e117c7ae00c45f78862bb80053257193d28f0ca9
albmp96 <92947424+AlbMP96@users.noreply.github.com>
2024-06-11 13:58:51 +0200

    register redirect

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -21,7 +21,7 @@ defineProps({
         class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50 w-screen"
     >
         <div
-            class="relative mx-2 flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
+            class="relative mx-2 flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white m-0"
         >
             <div
                 class="relative min-h-screen w-full max-w-2xl px-6 lg:max-w-7xl"
