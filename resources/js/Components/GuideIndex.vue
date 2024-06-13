5ecc501ca819d6e9bb355849ac254d852f2f2c80
albmp96 <92947424+AlbMP96@users.noreply.github.com>
2024-06-13 13:19:53 +0200

    titulo GuideIndex

--- resources/js/Components/GuideIndex.vue
+++ resources/js/Components/GuideIndex.vue
@@ -8,7 +8,7 @@ defineProps(["guide"]);
     <div
         class="border-red-600 border-solid border-2 bg-gray-800 flex flex-1 flex-col mx-auto my-2 p-2 text-center text-gray-200 sm:w-full md:w-1/2 rounded"
     >
-        <h1>
+        <h1 class="text-xl">
             <Link
                 class="underline hover:text-gray-400"
                 :href="route('showguide', guide.id)"
