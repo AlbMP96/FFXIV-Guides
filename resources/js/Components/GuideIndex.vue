1252e79716da7d9fcc5fa23e105e6f5d4de46de2
Alberto Muñiz <92947424+AlbMP96@users.noreply.github.com>
2024-06-13 17:36:32 +0200

    responsive

--- resources/js/Components/GuideIndex.vue
+++ resources/js/Components/GuideIndex.vue
@@ -8,7 +8,7 @@ defineProps(["guide"]);
     <div
         class="border-red-600 border-solid border-2 bg-gray-800 flex flex-1 flex-col mx-auto my-2 p-2 text-center text-gray-200 w-full lg:w-1/2 rounded"
     >
-        <h1 class="text-xl">
+        <h1 class="text-base sm:text-xl">
             <Link
                 class="underline hover:text-gray-400"
                 :href="route('showguide', guide.id)"
@@ -16,12 +16,12 @@ defineProps(["guide"]);
                 {{ guide.title }}
             </Link>
         </h1>
-        <div class="flex justify-between">
-            <p class="pt-2 me-3">Guide by: {{ guide.user.name }}</p>
+        <div class="flex justify-between text-sm sm:text-base">
+            <p class="pt-1 sm:pt-2 me-3">Guide by: {{ guide.user.name }}</p>
             <div class="flex">
-                <p class="p-2">{{ guide.ffclass.name }}</p>
+                <p class="pt-1 sm:pt-2">{{ guide.ffclass.name }}</p>
                 <img
-                    :class="`h-8 sm:h-12 border-2 border-yellow-300 rounded-md ms-2 ${
+                    :class="`h-6 sm:h-12 border-2 border-yellow-300 rounded-md ms-2 ${
                         guide.ffclass.role == 'Tank'
                             ? 'bg-blue-600'
                             : guide.ffclass.role == 'DPS'
